64f7f40ad5181eadbfdfbafa33e02767d5a36e60
Ariejan de Vroom <ariejan@devroom.io>
2018-11-05 22:29:17 +0100

    report vm.max_map_count in tests

--- tests/setup.sh
+++ tests/setup.sh
@@ -4,6 +4,9 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/test_helper.bash"
 
 BIN_STUBS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/bin"
 
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl vm.max_map_count
+
 if [[ ! -d $DOKKU_ROOT ]]; then
   git clone https://github.com/progrium/dokku.git $DOKKU_ROOT > /dev/null
 fi
@@ -32,4 +35,3 @@ if [[ ! -f $BIN_STUBS/plugn ]]; then
   find "$DOKKU_ROOT/plugins" -mindepth 1 -maxdepth 1 -type d ! -name 'available' ! -name 'enabled' -exec ln -s {} "$DOKKU_ROOT/plugins/enabled" \;
 fi
 
-sudo sysctl -w vm.max_map_count=262144
